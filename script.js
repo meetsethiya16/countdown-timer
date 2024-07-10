2d0ad6232c85ef2a8c35237f5cc36f89943bb585
Meet Sethiya <77146680+meetsethiya16@users.noreply.github.com>
2024-07-10 00:11:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const endDate = "11 July 2024, 12:00 AM";
+const endDate = "01 August 2024, 12:00 AM";
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input");
