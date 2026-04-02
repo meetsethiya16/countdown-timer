816f34877a14ac73b91e425050bb8dc11d863c1d
meetsethiya16 <meet.sethiya.16@gmail.com>
2026-04-02 11:43:08 +0530

    Refactor input handling in index.html and script.js for clarity

--- script.js
+++ script.js
@@ -36,10 +36,10 @@ function clock() {
 
   if (diff < 0) return;
 
-  inputs[0].value = Math.floor(diff / 3600 / 24);
-  inputs[1].value = Math.floor(diff / 3600) % 24;
-  inputs[2].value = Math.floor(diff / 60) % 60;
-  inputs[3].value = Math.floor(diff) % 60;
+  // inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[0].value = Math.floor(diff / 3600) % 24;
+  inputs[1].value = Math.floor(diff / 60) % 60;
+  inputs[2].value = Math.floor(diff) % 60;
 }
 
 setInterval(clock, 1000);
