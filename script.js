6f7e0551d20e92d0b60ca3bd669efeaef90333d6
Meet Sethiya <77146680+meetsethiya16@users.noreply.github.com>
2026-04-02 11:33:02 +0530

    Refactor end date handling to use dynamic time

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-const endDate = "1 April 2026, 06:00 PM";
+function getTodayEndTime() {
+  const now = new Date();
 
-document.getElementById("end-date").innerText = endDate;
+  const end = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(), // today's date
+    18, // 6 PM
+    0,
+    0,
+  );
+
+  return end;
+}
+
 const inputs = document.querySelectorAll("input");
 
 function clock() {
-  const end = new Date(endDate);
+  const end = getTodayEndTime();
   const now = new Date();
+
   const diff = (end - now) / 1000;
 
   if (diff < 0) return;
 
-  // convert into days;
   inputs[0].value = Math.floor(diff / 3600 / 24);
   inputs[1].value = Math.floor(diff / 3600) % 24;
   inputs[2].value = Math.floor(diff / 60) % 60;
   inputs[3].value = Math.floor(diff) % 60;
 }
 
-setInterval(clock, 100);
+setInterval(clock, 1000);
