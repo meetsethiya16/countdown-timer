1510fcd8f3f271b9174ca5d539b06ae2f68edf1c
Meet Sethiya <77146680+meetsethiya16@users.noreply.github.com>
2026-09-16 00:36:01 +0530

    Refactor countdown to use fixed end date
    Updated the countdown logic to use a fixed end date and adjusted the interval for the clock function.

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
+const endDate = "1 April 2026, 06:00 PM";
+
+document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input");
 
-function getTodayEndTime() {
-  const now = new Date();
-
-  return new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    17, // 10 PM
-    0,
-    0,
-  );
-}
-
-// Format date like: 2 April 2026, 10:00 PM
-function formatDate(date) {
-  return date.toLocaleString("en-IN", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
-}
-
 function clock() {
-  const end = getTodayEndTime();
+  const end = new Date(endDate);
   const now = new Date();
-
-  // 🔥 Update UI date
-  document.getElementById("end-date").innerText = formatDate(end);
-
   const diff = (end - now) / 1000;
 
   if (diff < 0) return;
 
-  // inputs[0].value = Math.floor(diff / 3600 / 24);
-  inputs[0].value = Math.floor(diff / 3600) % 24;
-  inputs[1].value = Math.floor(diff / 60) % 60;
-  inputs[2].value = Math.floor(diff) % 60;
+  // convert into days;
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
 }
 
-setInterval(clock, 1000);
+setInterval(clock, 100);
